862a01d2b941397a628f19218c2e5cd51a5a8c3f
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-03-30 13:47:44 +0200

    ts

--- AdminDash/src/app/app.component.ts
+++ AdminDash/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { query } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AdminDash';
+
+  ngOnInit(): void {
+      const block = document.querySelectorAll(".block") as NodeListOf<HTMLElement>;
+
+      block.forEach(block => {
+        const blockHeader = document.querySelector(".block-header") as HTMLElement;
+        const linkList = document.querySelector(".linkList") as HTMLElement;
+
+          blockHeader.addEventListener("click", () => {
+            const linkList = document.querySelector(".linkList") as HTMLElement;
+            linkList.classList.toggle("open-close");
+          })
+      })
+  }
 }
